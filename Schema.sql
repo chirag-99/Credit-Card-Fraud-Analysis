4da0d98ab0927b1808948e5d4b05884afc865540
Chirag Kariya <53505367+chirag-99@users.noreply.github.com>
2023-02-15 00:24:40 -0500

    Update Schema.sql

--- Schema.sql
+++ Schema.sql
@@ -1,25 +1,21 @@
 CREATE TABLE cardholder_details(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(40) NOT NULL)
-;
+    name VARCHAR(40) NOT NULL);
 
 CREATE TABLE credit_card_details(
 	card VARCHAR(30) NOT NULL PRIMARY KEY,
 	id_card_holder INT,
-	FOREIGN KEY (id_card_holder) REFERENCES cardholder_details(id))
-;
+	FOREIGN KEY (id_card_holder) REFERENCES cardholder_details(id));
 
 CREATE TABLE merchant_category(
 	id SERIAL PRIMARY KEY,
-	name VARCHAR(30) NOT NULL)
-;
+	name VARCHAR(30) NOT NULL);
 
 CREATE TABLE merchant_details(
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(30) NOT NULL,
 	id_merchant_category INT,
-	FOREIGN KEY (id_merchant_category) REFERENCES merchant_category(id))
-;
+	FOREIGN KEY (id_merchant_category) REFERENCES merchant_category(id));
 
 CREATE TABLE transactions(
 	id SERIAL,
@@ -29,5 +25,4 @@ CREATE TABLE transactions(
 	id_merchant INT,
 	PRIMARY KEY (id),
 	FOREIGN KEY (card) REFERENCES credit_card_details(card),
-	FOREIGN KEY (id_merchant) REFERENCES merchant_details(id))
-;
+	FOREIGN KEY (id_merchant) REFERENCES merchant_details(id));
